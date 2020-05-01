a76904752f358425b97480d9b0e54bb53947eba1
Maxime <mgaittet@mmt.ch>
2020-05-01 13:59:58 +0200

    Add demo buttons on MainActivity

--- app/src/main/java/com/mmt/realmfakedatademo/MainActivity.java
+++ app/src/main/java/com/mmt/realmfakedatademo/MainActivity.java
@@ -7,6 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import static com.mmt.fakedatalibrary.FakeDataGenerationService.enableActivity;
+import static com.mmt.fakedatalibrary.FakeDataGenerationService.enableHRV;
+import static com.mmt.fakedatalibrary.FakeDataGenerationService.enablePPG;
+import static com.mmt.fakedatalibrary.FakeDataGenerationService.enableWorkout;
+import static com.mmt.fakedatalibrary.FakeDataGenerationService.setFakeDataRefreshPeriodMs;
 import static com.mmt.fakedatalibrary.util.MMTfakeSleep.generateFakeSleepData;
 import static com.mmt.fakedatalibrary.util.StartStopService.startFakeDataService;
 import static com.mmt.fakedatalibrary.util.StartStopService.stopFakeDataService;
@@ -14,7 +19,7 @@ import static com.mmt.fakedatalibrary.util.Util.eraseAllRealmDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mBtnStartService, mBtnStopService, mBtnFakeSleep;
+    private Button mBtnStartService, mBtnStopService, mBtnFakeSleep, mBtnSetPeriod, mBtnEnableWorkout, mBtnDisableWorkout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         mBtnStartService = (Button) findViewById(R.id.button_start_service);
         mBtnStopService  = (Button) findViewById(R.id.button_stop_service);
         mBtnFakeSleep    = (Button) findViewById(R.id.button_fake_sleep);
+        mBtnSetPeriod = (Button) findViewById(R.id.button_set_period);
+        mBtnEnableWorkout = (Button) findViewById(R.id.button_enable_workout);
+        mBtnDisableWorkout = (Button) findViewById(R.id.button_disable_workout);
 
         mBtnStartService.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
                 // This function starts the background service which generates fake data every
                 // 1 minute (it can be adjusted with the PERIOD parameter in the FakeDataGerenationService)
                 // By default, only Activity and Hrv data are enabled. PPG and workout can be
-                // enabled with the PPG_ENABLED and WORKOUT_ENABLED (the have to be true to be enabled).
+                // enabled with enablePPG(true) and enableWorkout(true).
                 startFakeDataService(MainActivity.this, getApplicationContext());
             }
         });
@@ -55,6 +63,27 @@ public class MainActivity extends AppCompatActivity {
                 // This function has not been added in the FakeDataGenerationService, since the app
                 // can't get the data of the night regularly like the other metrics.
                 generateFakeSleepData();
+            }
+        });
+
+        mBtnSetPeriod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFakeDataRefreshPeriodMs(30*1000L);
+            }
+        });
+
+        mBtnEnableWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enableWorkout(true);
+            }
+        });
+
+        mBtnDisableWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enableWorkout(false);
             }
         });
     }
