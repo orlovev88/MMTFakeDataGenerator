3123d3bc19b6608aea978d7fac8e9445898ba606
Maxime <mgaittet@mmt.ch>
2020-05-05 09:17:50 +0200

    Implement getRawDataOlderOrEqualThanTimestamp and getRawDataNewerOrEqualThanTimestamp

--- fakedatalibrary/src/main/java/com/mmt/fakedatalibrary/util/RealmFakeData.java
+++ fakedatalibrary/src/main/java/com/mmt/fakedatalibrary/util/RealmFakeData.java
@@ -108,5 +108,101 @@ public class RealmFakeData {
 
         return results;
     }
+
+    public static RealmResults getRawDataOlderOrEqualThanTimestamp(DataTypeEnum dataTypeEnum, long timestampMax)
+    {
+        Realm realm;
+        realm = Realm.getDefaultInstance();
+        final String TIMESTAMP_FIELD = "timestamp";
+
+        RealmResults results;
+
+        switch(dataTypeEnum)
+        {
+            case ACTIVITY:
+                results = realm.where(MetricActivity.class)
+                        .lessThanOrEqualTo(TIMESTAMP_FIELD, timestampMax)
+                        .findAll();
+                break;
+
+            case HRV:
+                results = realm.where(MetricHRV.class)
+                        .lessThanOrEqualTo(TIMESTAMP_FIELD, timestampMax)
+                        .findAll();
+                break;
+
+            case PPG:
+                results = realm.where(MetricPPG.class)
+                        .lessThanOrEqualTo(TIMESTAMP_FIELD, timestampMax)
+                        .findAll();
+                break;
+
+            case SLEEP:
+                results = realm.where(MetricSleep.class)
+                        .lessThanOrEqualTo(TIMESTAMP_FIELD, timestampMax)
+                        .findAll();
+                break;
+
+            case WORKOUT:
+                results = realm.where(MetricWorkout.class)
+                        .lessThanOrEqualTo(TIMESTAMP_FIELD, timestampMax)
+                        .findAll();
+                break;
+
+            default:
+                results = null;
+                break;
+        }
+
+        return results;
+    }
+
+    public static RealmResults getRawDataNewerOrEqualThanTimestamp(DataTypeEnum dataTypeEnum, long timestampMin)
+    {
+        Realm realm;
+        realm = Realm.getDefaultInstance();
+        final String TIMESTAMP_FIELD = "timestamp";
+
+        RealmResults results;
+
+        switch(dataTypeEnum)
+        {
+            case ACTIVITY:
+                results = realm.where(MetricActivity.class)
+                        .greaterThanOrEqualTo(TIMESTAMP_FIELD, timestampMin)
+                        .findAll();
+                break;
+
+            case HRV:
+                results = realm.where(MetricHRV.class)
+                        .greaterThanOrEqualTo(TIMESTAMP_FIELD, timestampMin)
+                        .findAll();
+                break;
+
+            case PPG:
+                results = realm.where(MetricPPG.class)
+                        .greaterThanOrEqualTo(TIMESTAMP_FIELD, timestampMin)
+                        .findAll();
+                break;
+
+            case SLEEP:
+                results = realm.where(MetricSleep.class)
+                        .greaterThanOrEqualTo(TIMESTAMP_FIELD, timestampMin)
+                        .findAll();
+                break;
+
+            case WORKOUT:
+                results = realm.where(MetricWorkout.class)
+                        .greaterThanOrEqualTo(TIMESTAMP_FIELD, timestampMin)
+                        .findAll();
+                break;
+
+            default:
+                results = null;
+                break;
+        }
+
+        return results;
+    }
     
 }
